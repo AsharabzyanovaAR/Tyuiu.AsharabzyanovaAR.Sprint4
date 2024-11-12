a19ac32f13e3738c62bbb100b5fbe718cad13678
AsharabzyanovaAR <rustamovna14082006@gmail.com>
2024-11-12 23:51:12 +0500

    Task3

---
+++ Tyuiu.AsharabzyanovaAR.Sprint4.Task3.V6.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿using System.Numerics;
+using System.Runtime.InteropServices;
+using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.AsharabzyanovaAR.Sprint4.Task3.V6.Lib
+{
+    public class DataService : ISprint4Task3V6
+    {
+        public int Calculate(int[,] array)
+        {
+            int rows = array.GetUpperBound(0) + 1;
+            int columns = array.Length / rows;
+            int count = 0;
+            int max=-100000;
+            
+
+            for(int i=0;i< rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (array[i, j] > max && i==1)
+                    {
+                        max = array[i, j];
+
+                    }
+                }
+            }
+            return max;
+        }
+    }
+}
---
+++ Tyuiu.AsharabzyanovaAR.Sprint4.Task3.V6/Program.cs
@@ -0,0 +1,53 @@
+﻿using Tyuiu.AsharabzyanovaAR.Sprint4.Task3.V6.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[,] mas = new int[5, 5] {{8, 8, 3, 4, 5 },
+
+                                     { 8, 6, 6, 4, 6 },
+
+                                     { 3, 6, 5, 3, 4 },
+
+                                     { 5, 6, 3, 7, 5 },
+
+                                     { 7, 8, 5, 6, 6 } };
+
+        int rows = mas.GetUpperBound(0) + 1;
+        int columns = mas.Length / rows;
+        DataService ds = new DataService();
+        Console.Title = ("Спринт #3 | Выполнила: Ашарабзянова А.Р. | ПКТб-24-1");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Двумерные массивы. (статический ввод)                             *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #6                                                              *");
+        Console.WriteLine("* Выполнила: Ашарабзянова Алина Рустамовна | ПКТб-24-1                    *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов.                    *");
+        Console.WriteLine("* Найдите максимальный элемент во второй строке массива.                  *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Массив:");
+        for(int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.WriteLine($"{mas[i, j]}\t");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        int res = ds.Calculate(mas);
+        Console.WriteLine("Максимальное число-" + res);
+        Console.ReadKey();
+    }
+}
