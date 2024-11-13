eb44cc6ea757f8a194ecba3634b0d8335798d356
AsharabzyanovaAR <rustamovna14082006@gmail.com>
2024-11-14 00:48:30 +0500

    Task6

---
+++ Tyuiu.AsharabzyanovaAR.Sprint4.Task6.V21.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.AsharabzyanovaAR.Sprint4.Task6.V21.Lib;
+namespace Tyuiu.AsharabzyanovaAR.Sprint4.Task6.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            var v = new string[] { "Facebook", "Twitter", "Instagram", "Snapchat", "LinkedIn", "Pinterest", "Reddit" };
+            int res = ds.Calculate(v);
+            int wait = 2;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.AsharabzyanovaAR.Sprint4.Task6.V21/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.AsharabzyanovaAR.Sprint4.Task6.V21.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Random rnd = new Random();
+        Console.Title = ("Спринт #4 | Выполнила: Ашарабзянова А.Р. | ПКТб-24-1");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Класс Array                                                       *");
+        Console.WriteLine("* Задание #6                                                              *");
+        Console.WriteLine("* Вариант #21                                                             *");
+        Console.WriteLine("* Выполнила: Ашарабзянова Алина Рустамовна | ПКТб-24-1                    *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Подсчитать количество элементов, длина которых меньше 8                 *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        var v = new string[] { "Facebook", "Twitter", "Instagram", "Snapchat", "LinkedIn", "Pinterest", "Reddit" };
+        Console.WriteLine("Исходный массив");
+        for(int i = 0; i <= v.Length - 1; i++)
+        {
+            Console.WriteLine(v[i]);
+        }
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Количество элементов длина которых меньше 8:");
+        int nums = ds.Calculate(v);
+        Console.WriteLine(nums);
+        Console.ReadKey();
+    }
+}
